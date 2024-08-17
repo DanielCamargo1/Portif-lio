0bbb7dfbf99031aaefb8a56f5c88144d27277857
DanielCamargo <danidcp098@gmail.com>
2024-08-17 16:06:20 -0300

    imagem dos projetos

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 import 'boxicons'
+
+
+
+const projetos = document.querySelectorAll('.projetos-item');
+
+projetos.forEach(projeto => {
+    projeto.addEventListener('mouseover', () => {
+        projeto.style.transform = 'scale(1.05)'; 
+        projeto.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)'; // Adiciona sombra
+    });
+
+    projeto.addEventListener('mouseout', () => {
+        projeto.style.transform = 'scale(1)'; 
+        projeto.style.boxShadow = 'none'; 
+    });
+});
